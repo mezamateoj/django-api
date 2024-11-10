bcdffc868863cfe0e03b753c4ed09dca14a5d5db
mateo meza <mmeza@microsystem.cl>
2024-11-10 13:01:43 -0300

    Add: view set methods

--- app/hello/urls.py
+++ app/hello/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+# registering viewset views
+router = DefaultRouter()
+router.register('api/hello-viewset', views.HelloViewSet, basename='hello-viewset')
+
 urlpatterns = [
     path('api/hello', views.HelloView.as_view(), name='hello-view'),
+    path('', include(router.urls))
 ]
